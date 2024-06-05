1716ef774b604da64274f22dd0660bf9740c91f5
Yermukhammed Assankhan <assankhan_y@mail.ru>
2024-06-05 19:34:06 +0500

    TTTI-2 Добавить ViewModel
    Добавил ViewModel, перенес логику игры в ViewModel

--- app/src/main/java/com/example/tictactoeinfinity/FirstFragment.kt
+++ app/src/main/java/com/example/tictactoeinfinity/FirstFragment.kt
@@ -1,10 +1,11 @@
 package com.example.tictactoeinfinity
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.tictactoeinfinity.databinding.FragmentFirstBinding
 
--- app/src/main/java/com/example/tictactoeinfinity/SecondFragment.kt
+++ app/src/main/java/com/example/tictactoeinfinity/SecondFragment.kt
@@ -1,15 +1,12 @@
 package com.example.tictactoeinfinity
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.GridLayout
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
 import com.example.tictactoeinfinity.databinding.FragmentSecondBinding
 
 /**
@@ -19,13 +16,8 @@ class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
-    private lateinit var board: Array<Array<Player?>>
-    private lateinit var buttons: Array<Array<ImageButton>>
-    private var currentPlayer = Player.X
-    private val moves = mutableListOf<Pair<Int, Int>>()
+    val viewModel: MainViewModel by viewModels()
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -35,6 +27,46 @@ class SecondFragment : Fragment() {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         setupBoard()
+
+        viewModel.getShowDialogLiveData().observe(viewLifecycleOwner) { message ->
+            if (message.isEmpty())
+                return@observe
+
+            showWinDialog(message)
+        }
+
+        viewModel.getButtonsLiveData().observe(viewLifecycleOwner) {
+
+            binding.button00.setImageResource(it[0][0].imageRes)
+            binding.button00.alpha = it[0][0].alpha
+
+            binding.button01.setImageResource(it[0][1].imageRes)
+            binding.button01.alpha = it[0][1].alpha
+
+            binding.button02.setImageResource(it[0][2].imageRes)
+            binding.button02.alpha = it[0][2].alpha
+
+
+            binding.button10.setImageResource(it[1][0].imageRes)
+            binding.button10.alpha = it[1][0].alpha
+
+            binding.button11.setImageResource(it[1][1].imageRes)
+            binding.button11.alpha = it[1][1].alpha
+
+            binding.button12.setImageResource(it[1][2].imageRes)
+            binding.button12.alpha = it[1][2].alpha
+
+
+            binding.button20.setImageResource(it[2][0].imageRes)
+            binding.button20.alpha = it[2][0].alpha
+
+            binding.button21.setImageResource(it[2][1].imageRes)
+            binding.button21.alpha = it[2][1].alpha
+
+            binding.button22.setImageResource(it[2][2].imageRes)
+            binding.button22.alpha = it[2][2].alpha
+        }
+
         return binding.root
 
     }
@@ -42,80 +74,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonRestart.setOnClickListener {
-            resetBoard()
+            viewModel.resetBoard()
         }
-    }
-
-    private fun setupBoard() {
-        board = Array(3) { arrayOfNulls<Player>(3) }
-        buttons = arrayOf(
-            arrayOf(binding.button00, binding.button01, binding.button02),
-            arrayOf(binding.button10, binding.button11, binding.button12),
-            arrayOf(binding.button20, binding.button21, binding.button22)
-        )
-
-        for (row in 0..2) {
-            for (col in 0..2) {
-                buttons[row][col].setOnClickListener { onCellClicked(row, col) }
-            }
-        }
-    }
-
-    private fun onCellClicked(row: Int, col: Int) {
-        if (board[row][col] == null) {
-            buttons[row][col].setImageResource(currentPlayer.imageRes)
-            board[row][col] = currentPlayer
-            moves.add(Pair(row, col))
-
-            if (moves.size > 6) {
-                val (oldRow, oldCol) = moves.removeAt(0)
-                board[oldRow][oldCol] = null
-                buttons[oldRow][oldCol].setImageResource(0)
-            }
-
-            updateButtonAlpha()
-
-            if (checkWin(currentPlayer)) {
-                Toast.makeText(requireContext(), "Win ${currentPlayer.symbol}", Toast.LENGTH_SHORT).show()
-                resetBoard()
-            } else {
-                currentPlayer = if (currentPlayer == Player.X) Player.O else Player.X
-            }
-        }
-    }
-
-    private fun updateButtonAlpha() {
-        for (i in moves.indices) {
-            val (row, col) = moves[i]
-            buttons[row][col].alpha = when (i) {
-                moves.size - 4 -> 0.9f
-                moves.size - 5 -> 0.8f
-                moves.size - 6 -> 0.5f
-                else -> 1f
-            }
-        }
-    }
-
-    private fun checkWin(player: Player): Boolean {
-        for (i in 0..2) {
-            if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
-                (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-            ) return true
-        }
-        return (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-                (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-    }
-
-    private fun resetBoard() {
-        board = Array(3) { arrayOfNulls<Player>(3) }
-        for (row in 0..2) {
-            for (col in 0..2) {
-                buttons[row][col].setImageResource(0)
-                buttons[row][col].alpha = 1.0f
-            }
-        }
-        moves.clear()
-        currentPlayer = Player.X
     }
 
     override fun onDestroyView() {
@@ -123,8 +83,48 @@ class SecondFragment : Fragment() {
         _binding = null
     }
 
-    enum class Player(val symbol: String, val imageRes: Int) {
-        X("X", R.drawable.x_image),
-        O("O", R.drawable.o_image)
+
+    private fun showWinDialog(winner: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Game Over")
+        builder.setMessage("$winner wins")
+        builder.setIcon(R.drawable.icon_tictactoe)
+        builder.setPositiveButton("Restart") { dialog, _ ->
+            dialog.dismiss()
+            viewModel.dissmissed()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun setupBoard() {
+
+        binding.button00.setOnClickListener {
+            viewModel.onCellClicked(0, 0)
+        }
+        binding.button01.setOnClickListener {
+            viewModel.onCellClicked(0, 1)
+        }
+        binding.button02.setOnClickListener {
+            viewModel.onCellClicked(0, 2)
+        }
+        binding.button10.setOnClickListener {
+            viewModel.onCellClicked(1, 0)
+        }
+        binding.button11.setOnClickListener {
+            viewModel.onCellClicked(1, 1)
+        }
+        binding.button12.setOnClickListener {
+            viewModel.onCellClicked(1, 2)
+        }
+        binding.button20.setOnClickListener {
+            viewModel.onCellClicked(2, 0)
+        }
+        binding.button21.setOnClickListener {
+            viewModel.onCellClicked(2, 1)
+        }
+        binding.button22.setOnClickListener {
+            viewModel.onCellClicked(2, 2)
+        }
     }
 }
